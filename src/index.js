c85637fe536c1ea48304a5457dea493c37beaffa
Alex <alex.formail@gmail.com>
2018-01-17 22:54:26 +0300

    solution is added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function trapRainWater(elevationMap) {
-    // your solution here
+  const input = elevationMap;
+
+  const isSolid = (x, y) => input[x] < y;
+
+  const x = input.length;
+  const y = Math.max( ...input );
+
+  const full = x * y;
+  const solid = input.reduce((a, b) => a + b, 0);
+
+  let j = 1;
+  let free = 0;
+  let i = 0;
+
+  for (let j = 1; j <= y; j++) {
+    i = 0;
+    while (isSolid(i, j)) {
+      free++;
+      i++;
+    }
+    i = x - 1;
+    while (isSolid(i, j)) {
+      free++;
+      i--;
+    }
+  }
+
+  return full - (solid + free);
 }
